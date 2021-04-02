1e9d4eae4423ebf7753392361bbe3646f2819a50
Hun Lam <hunlam97@gmail.com>
2021-04-02 11:58:50 +0700

    fix: credentials

--- connector/RestConnector.ts
+++ connector/RestConnector.ts
@@ -2,4 +2,5 @@ import axios, { AxiosInstance } from "axios";
 
 export const restConnector: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
 });
